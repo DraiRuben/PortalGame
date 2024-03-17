34300f45d8615ab13cfd2600d54fd3467075cada
DraiRuben <r.drai@lahorde.school>
2024-03-17 18:06:12 +0100

    Object duplication almost done

---
+++ Plugins/Portals/Source/Portals/Private/UDuplicationData.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UDuplicationData.h"
+
+UDuplicationData::UDuplicationData()
+{
+}
+
+UDuplicationData::~UDuplicationData()
+{
+}
+
+
+UDuplicationData::UDuplicationData(AActor* Portal, AActor* Object)
+	: Portal(Portal), Object(Object)
+{
+}
+
+UDuplicationData* UDuplicationData::MakeDuplicationData(AActor* newPortal, AActor* newObject)
+{
+	UDuplicationData* Data = NewObject<UDuplicationData>();
+	Data->Portal = newPortal;
+	Data->Object = newObject;
+	return Data;
+}
